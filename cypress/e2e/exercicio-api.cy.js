d339c96c4354f69722d224659978a23773ed4908
Gabriel Mai <gabriel.mai@belluno.digital>
2024-08-21 11:13:18 -0300

    feat: Add API tests for user functionality

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -1,29 +1,64 @@
 /// <reference types="cypress" />
+import UsuariosPage from "../support/page-objects/usuarios.page";
+import UsuariosContract from "../contracts/usuarios.contract";
 
 describe('Testes da Funcionalidade Usuários', () => {
 
+  before(() => {
+    UsuariosPage.obterToken();
+  });
+
   it('Deve validar contrato de usuários', () => {
-    //TODO: 
+    UsuariosPage.listarUsuarios().then((response) => {
+      UsuariosContract.validateAsync(response.body);
+    });
   });
 
   it('Deve listar usuários cadastrados', () => {
-    //TODO: 
+    UsuariosPage.listarUsuarios().then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body.usuarios).to.be.an('array')
+      expect(response.body.usuarios.length).to.eq(response.body.quantidade)
+    });
   });
 
   it('Deve cadastrar um usuário com sucesso', () => {
-    //TODO: 
+    UsuariosPage.criarUsuario(UsuariosPage.criarUsuarioAleatorio()).then((response) => {
+      expect(response.status).to.eq(201)
+      expect(response.body.message).to.eq('Cadastro realizado com sucesso')
+    });
   });
 
   it('Deve validar um usuário com email inválido', () => {
-    //TODO: 
+    UsuariosPage.obterUsuarioAleatorio().then((usuario) => {
+      const usuarioInvalido = UsuariosPage.criarUsuarioAleatorio();
+      usuarioInvalido.email = usuario.email;
+
+        UsuariosPage.criarUsuario(usuarioInvalido).then((response) => {
+            expect(response.status).to.eq(400)
+            expect(response.body.message).to.eq('Este email já está sendo usado')
+        });
+
+    });
   });
 
   it('Deve editar um usuário previamente cadastrado', () => {
-    //TODO: 
+    UsuariosPage.obterUsuarioAleatorio().then((usuario) => {
+      UsuariosPage.editarUsuario(usuario._id, UsuariosPage.criarUsuarioAleatorio()).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.message).to.eq('Registro alterado com sucesso')
+      });
+    });
+
   });
 
   it('Deve deletar um usuário previamente cadastrado', () => {
-    //TODO: 
+    UsuariosPage.obterUsuarioAleatorio().then((usuario) => {
+      UsuariosPage.deletarUsuario(usuario._id).then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.message).to.eq('Registro excluído com sucesso')
+      });
+    });
   });
 
 
