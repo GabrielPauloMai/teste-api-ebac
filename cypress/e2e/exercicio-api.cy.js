b47ec2516bc7ccbe4fd22e8d8683a091f49f76a1
Gabriel Mai <gabriel.mai@belluno.digital>
2024-08-25 16:55:02 -0300

    refactor: Refactor API tests for user functionality

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -1,21 +1,20 @@
 /// <reference types="cypress" />
-import UsuariosPage from "../support/page-objects/usuarios.page";
 import UsuariosContract from "../contracts/usuarios.contract";
 
 describe('Testes da Funcionalidade Usuários', () => {
 
   before(() => {
-    UsuariosPage.obterToken();
+    cy.obterToken();
   });
 
   it('Deve validar contrato de usuários', () => {
-    UsuariosPage.listarUsuarios().then((response) => {
+    cy.listarUsuarios().then((response) => {
       UsuariosContract.validateAsync(response.body);
     });
   });
 
   it('Deve listar usuários cadastrados', () => {
-    UsuariosPage.listarUsuarios().then((response) => {
+    cy.listarUsuarios().then((response) => {
       expect(response.status).to.eq(200)
       expect(response.body.usuarios).to.be.an('array')
       expect(response.body.usuarios.length).to.eq(response.body.quantidade)
@@ -23,38 +22,40 @@ describe('Testes da Funcionalidade Usuários', () => {
   });
 
   it('Deve cadastrar um usuário com sucesso', () => {
-    UsuariosPage.criarUsuario(UsuariosPage.criarUsuarioAleatorio()).then((response) => {
-      expect(response.status).to.eq(201)
-      expect(response.body.message).to.eq('Cadastro realizado com sucesso')
+    cy.criarUsuarioAleatorio().then((usuario) => {
+        cy.criarUsuario(usuario).then((response) => {
+            expect(response.status).to.eq(201)
+            expect(response.body.message).to.eq('Cadastro realizado com sucesso')
+        });
     });
+
   });
 
   it('Deve validar um usuário com email inválido', () => {
-    UsuariosPage.obterUsuarioAleatorio().then((usuario) => {
-      const usuarioInvalido = UsuariosPage.criarUsuarioAleatorio();
-      usuarioInvalido.email = usuario.email;
-
-        UsuariosPage.criarUsuario(usuarioInvalido).then((response) => {
+    cy.criarUsuarioAleatorio().then((usuario) => {
+        usuario.email = 'emailinvalido';
+        cy.criarUsuario(usuario).then((response) => {
             expect(response.status).to.eq(400)
-            expect(response.body.message).to.eq('Este email já está sendo usado')
+            expect(response.body.email).to.eq('email deve ser um email válido')
         });
-
     });
   });
 
   it('Deve editar um usuário previamente cadastrado', () => {
-    UsuariosPage.obterUsuarioAleatorio().then((usuario) => {
-      UsuariosPage.editarUsuario(usuario._id, UsuariosPage.criarUsuarioAleatorio()).then((response) => {
-        expect(response.status).to.eq(200)
-        expect(response.body.message).to.eq('Registro alterado com sucesso')
+    cy.obterUsuarioAleatorio().then((usuario) => {
+      cy.criarUsuarioAleatorio().then((novoUsuario) => {
+        cy.editarUsuario(usuario._id, novoUsuario).then((response) => {
+          expect(response.status).to.eq(200)
+          expect(response.body.message).to.eq('Registro alterado com sucesso')
+        });
       });
     });
 
   });
 
   it('Deve deletar um usuário previamente cadastrado', () => {
-    UsuariosPage.obterUsuarioAleatorio().then((usuario) => {
-      UsuariosPage.deletarUsuario(usuario._id).then((response) => {
+    cy.obterUsuarioAleatorio().then((usuario) => {
+      cy.deletarUsuario(usuario._id).then((response) => {
         expect(response.status).to.eq(200)
         expect(response.body.message).to.eq('Registro excluído com sucesso')
       });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,28 +1,113 @@
-Cypress.Commands.add('token', (email, senha) => {
+const admin = require('../fixtures/administrador.json');
+import {faker} from '@faker-js/faker';
+
+/**
+ * Comando para obter o token de autorização
+ * @returns {void}
+ */
+Cypress.Commands.add('obterToken', () => {
     cy.request({
         method: 'POST',
         url: 'login',
         body: {
-            "email": email,
-            "password": senha 
-        }
+            email: admin.email,
+            password: admin.password
+        },
     }).then((response) => {
-        expect(response.status).to.equal(200)
-        return response.body.authorization
-    })
- })
+        Cypress.env('token', response.body.authorization);
+    });
+});
 
- Cypress.Commands.add('cadastrarProduto' , (token, produto, preco, descricao, quantidade) =>{
-    cy.request({
-        method: 'POST', 
-        url: 'produtos',
-        headers: {authorization: token}, 
-        body: {
-            "nome": produto,
-            "preco": preco,
-            "descricao": descricao,
-            "quantidade": quantidade
-          }, 
-          failOnStatusCode: false
+/**
+ * Comando para listar os usuários
+ * @returns {Response}
+ */
+
+Cypress.Commands.add('listarUsuarios', () => {
+  return cy.request({
+    method: 'GET',
+    url: 'usuarios',
+  }).then((response) => {
+    return {
+      status: response.status,
+      body: response.body,
+      quantidade: response.body.quantidade
+    };
+  });
+});
+
+/**
+ * Comando para criar um usuário
+ * @param {Usuario} usuario
+ * @returns {Response}
+ */
+
+Cypress.Commands.add('criarUsuario', (usuario) => {
+    return cy.request({
+      method: 'POST',
+      url: 'usuarios',
+      headers: { authorization: Cypress.env('token') },
+      body: usuario,
+      failOnStatusCode: false
     })
- })
+});
+
+/**
+ * Comando para editar um usuário
+ * @param {string} usuario_id
+ * @param {Usuario} usuario
+ * @returns {Response}
+ * */
+
+Cypress.Commands.add('editarUsuario', (usuario_id, usuario) => {
+    return cy.request({
+      method: 'PUT',
+      url: `usuarios/${usuario_id}`,
+      headers: { authorization: Cypress.env('token') },
+      body: usuario,
+      failOnStatusCode: false
+    });
+});
+
+/**
+ * Comando para deletar um usuário
+ * @param {string} usuario_id
+ * @returns {Response}
+ */
+Cypress.Commands.add('deletarUsuario', (usuario_id) => {
+    return cy.request({
+      method: 'DELETE',
+      url: `usuarios/${usuario_id}`,
+      headers: { authorization: Cypress.env('token') },
+      failOnStatusCode: false
+    });
+});
+
+/**
+ * Comando auxiliar para criar um usuário aleatório
+ * @returns {Usuario}
+ * */
+
+Cypress.Commands.add('criarUsuarioAleatorio', () => {
+  let nome = faker.person.fullName();
+  return cy.wrap({
+    nome: nome,
+    email: faker.internet.email(
+      nome.split(' ')[0],
+      nome.split(' ')[1],
+    ),
+    password: faker.internet.password(),
+    administrador: faker.datatype.boolean().toString(),
+  });
+});
+
+
+/**
+ * Comando auxiliar para obter um usuário aleatório
+ * @returns {Usuario}
+ */
+Cypress.Commands.add('obterUsuarioAleatorio', () => {
+  return cy.listarUsuarios().then((response) => {
+      return response.body.usuarios[Math.floor(Math.random() * response.body.quantidade)];
+  });
+});
